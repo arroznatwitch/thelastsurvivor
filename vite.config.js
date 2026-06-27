a2cd217f3fe9864145fe9129bfa82bd8f9893fe5
Cardoso <rui.pedro.goncalves.cardoso@gmail.com>
2026-06-27 15:25:06 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Domínio próprio (playthelastsurvivor.com) serve na raiz
+  base: '/', 
 })
